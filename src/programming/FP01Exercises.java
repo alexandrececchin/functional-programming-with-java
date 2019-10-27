d8f9d6e89bb0c4694b14a4b38c1e93626006e90f
Alexandre Rocha <alexandrececchin@gmail.com>
2019-10-27 15:59:05 -0300

    #FP01 - Exercises 05,06 using map

--- src/programming/FP01Exercises.java
+++ src/programming/FP01Exercises.java
@@ -4,19 +4,27 @@ import java.util.List;
 
 public class FP01Exercises {
     public static void main(String[] args) {
-//        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 //        printOddNumbersInListFunctional(numbers);
-
+        printCubeOfOddNumbersInListFunctional(numbers);
 
         List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
 //        courses.forEach(System.out::println);
 
 //        courses.stream().filter(word -> word.contains("Spring")).forEach(System.out::println);
-        courses.stream().filter(word -> word.length() >= 4).forEach(System.out::println);
-
+//        courses.stream().filter(word -> word.length() >= 4).forEach(System.out::println);
+        courses.stream().map(course -> course + " " + course.length()).forEach(System.out::println);
     }
 
     private static void printOddNumbersInListFunctional(List<Integer> numbers) {
         numbers.stream().filter(n -> n % 2 != 0).forEach(System.out::println);
     }
+
+    private static void printCubeOfOddNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(n -> n % 2 != 0)
+                .map(n -> n * n * n)
+                .forEach(System.out::println);
+    }
+
 }
