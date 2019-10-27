a7edaea9da84644e2f2c44fc8d592577877a4e03
Alexandre Rocha <alexandrececchin@gmail.com>
2019-10-27 15:49:08 -0300

    #FP01 - Exercise 02,03,04 print courses list

--- src/programming/FP01Exercises.java
+++ src/programming/FP01Exercises.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 public class FP01Exercises {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//        printOddNumbersInListFunctional(numbers);
 
-        printOddNumbersInListFunctional(numbers);
+
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+//        courses.forEach(System.out::println);
+
+//        courses.stream().filter(word -> word.contains("Spring")).forEach(System.out::println);
+        courses.stream().filter(word -> word.length() >= 4).forEach(System.out::println);
+
     }
 
     private static void printOddNumbersInListFunctional(List<Integer> numbers) {
-        numbers.stream().filter( n -> n % 2 != 0).forEach(System.out::println);
+        numbers.stream().filter(n -> n % 2 != 0).forEach(System.out::println);
     }
 }
