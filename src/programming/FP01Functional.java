141e10982b8a49fcccbad404cacbaf62b0756bdb
Alexandre Rocha <alexandrececchin@gmail.com>
2019-10-27 15:58:20 -0300

    #FP01 - Using Stream Map

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -7,7 +7,15 @@ public class FP01Functional {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 //        printAllNumbersInListFunctional(numbers);
-        printEvenNumbersInListFunctional(numbers);
+//        printEvenNumbersInListFunctional(numbers);
+        printSquareOfEvenNumbersInListFunctional(numbers);
+    }
+
+    private static void printSquareOfEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(number -> number * number)
+                .forEach(System.out::println);
     }
 
     private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
