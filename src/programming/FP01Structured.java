618347aca679306e588f6b88e31edda58982f203
Alexandre Rocha <alexandrececchin@gmail.com>
2019-10-27 15:25:00 -0300

    #FP01 - Print list of numbers

---
+++ src/programming/FP01Structured.java
@@ -0,0 +1,18 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Structured {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printAllNumbersInListStructured(numbers);
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        for (Integer number : numbers)
+            System.out.println(number);
+    }
+
+
+}
